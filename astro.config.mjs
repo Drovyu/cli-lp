d701a3833ef4d4d574064e5a1ece07a1b085bb66
sakuyasann <info@sakuyasan.net>
2026-07-11 13:59:05 +0900

    fix: make sitemap build platform independent

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://cli.drovyu.com',
   output: 'static',
-  integrations: [sitemap()],
   build: {
     // Serve fewer requests from the Worker: embed all CSS into the HTML.
     inlineStylesheets: 'always',
